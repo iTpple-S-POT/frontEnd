cbc1acd62c0cb7c8b8a0262fcb95478436b83110
J0onYEong <cjy2134@naver.com>
2023-12-23 21:41:03 +0900

    [Feat] MainScreen 구성요소 레이아웃배분 초기구현

---
+++ Packages/MainScreenUI/Sources/MainScreenUI/Screen/MainScreen.swift
@@ -0,0 +1,42 @@
+//
+//  MainScreen.swift
+//
+//
+//  Created by 최준영 on 2023/12/23.
+//
+
+import SwiftUI
+
+struct MainScreen: View {
+    var body: some View {
+        VStack(spacing: 0) {
+            
+            // 서치바 + 타이틀
+            Rectangle()
+                .fill(.red)
+                .frame(height: 56)
+            
+            // 태그 선택 바
+            Rectangle()
+                .fill(.orange)
+                .frame(height: 64)
+            
+            // 팟을 표시하는 지도
+            ZStack {
+                Rectangle()
+                    .fill(.white)
+                Text("map")
+            }
+            
+            // 탭뷰
+            Rectangle()
+                .fill(.green)
+                .frame(height: 64)
+            
+        }
+    }
+}
+
+#Preview {
+    MainScreen()
+}
