da9da8b52d3eacdeed5491bad3e05bd3b1bff839
J0onYEong <cjy2134@naver.com>
2023-12-29 16:53:42 +0900

    [Feat] 선택한 사진을 표시하는 화면 추가

--- Packages/MainScreenUI/Sources/MainScreenUI/Screen/PotUploadScreen.swift
+++ Packages/MainScreenUI/Sources/MainScreenUI/Screen/PotUploadScreen.swift
@@ -11,86 +11,49 @@ import CJPhotoCollection
 
 struct PotUploadScreen: View {
     
-    
+    @Environment(\.dismiss) var dismiss
     @StateObject private var screenModel = PotUploadScreenModel()
     
     
     var body: some View {
-        VStack(spacing: 0) {
-            
-            ZStack {
-                
-                if screenModel.authorizationStatus == .authorized {
-                    
-                    Text("모든 사진에대한 접근권한")
-                    
-                }
-                
-                if screenModel.authorizationStatus == .limited {
-                    
-                    Text("제한된 접근")
-                    
-                }
-                
-                if screenModel.authorizationStatus == .restricted {
-                    
-                    Text("사진앱에 접근 불가한 디바이스입니다.")
-                    
-                }
-                
-                if screenModel.authorizationStatus == .denied {
-                    
-                    Text("접근이 제한되었습니다.")
-                    
-                }
-                
-            }
-            .frame(height: 100)
-            
-            Spacer(minLength: 0)
-            
-            if screenModel.authorizationStatus == .authorized || screenModel.authorizationStatus == .limited {
-                
-                CJPhotoCollectionView { image in
-                    
-                    screenModel.selectedImage = image
-                    
-                }
-                
-            } else {
-                
-                GeometryReader(content: { geo in
-                    Text("접근할 수 있는 사진이 없습니다.")
-                        .position(CGPoint(x: geo.size.width/2, y: geo.size.height/2))
-                })
-                
-            }
-            
-        }
-        .onAppear(perform: {
-            screenModel.checkAuthorizationStatus()
-        })
         
-    }
-    
-}
-
-class PotUploadScreenModel: ObservableObject {
-    
-    @Published private(set) var authorizationStatus = PHPhotoLibrary.authorizationStatus(for: .readWrite)
-    
-    @Published var selectedImage: ImageInformation?
-    
-    func checkAuthorizationStatus() {
-        
-        // notDetermined상태인 경우만 권한요청을 보냄
-        if authorizationStatus == .notDetermined {
+        NavigationStack(path: $screenModel.navigationStack) {
             
-            PHPhotoLibrary.requestAuthorization(for: .readWrite) { newStatus in
+            VStack(spacing: 0) {
+                HStack {
+                    Spacer()
+                    
+                    Button {
+                        
+                        dismiss()
+                        
+                    } label: {
+                        
+                        Image(systemName: "x.circle")
+                            .resizable()
+                            .scaledToFit()
+                            .foregroundStyle(.black)
+                        
+                    }
+                    .padding(.trailing, 20)
+                }
+                .frame(height: 50)
                 
-                DispatchQueue.main.async { self.authorizationStatus = newStatus }
-                
+                SelectPhotoScreenComponent(screenModel: screenModel)
             }
+            
+                .navigationDestination(for: DestinationSC.self) { type in
+                    
+                    switch type {
+                    case .editPotSC:
+                        Image(uiImage: screenModel.photoInfo?.image ?? UIImage(systemName: "x.circle")!)
+                            .resizable()
+                            .scaledToFit()
+                    case .selectPhotoSc:
+                        fatalError("Navigation Error")
+                    }
+                    
+                }
             
         }
         
