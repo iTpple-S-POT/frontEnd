1972b2e97c4d5f91858e8402d409e495422b2e8d
J0onYEong <cjy2134@naver.com>
2024-02-19 08:58:23 +0900

    [Feat] 마이페이지스크린 네비게이션뷰로 변경

--- Packages/MainScreenUI/Sources/MainScreenUI/Screen/TabScreens/MyPageScreen/MyPageScreen.swift
+++ Packages/MainScreenUI/Sources/MainScreenUI/Screen/TabScreens/MyPageScreen/MyPageScreen.swift
@@ -19,119 +19,123 @@ struct MyPageScreen: View {
     @State private var showProfileDetail = false
     
     var body: some View {
-        ZStack {
-            
-            Color.white.ignoresSafeArea(.all, edges: .top)
-            
-            ScrollView {
+        NavigationView {
+            ZStack {
                 
-                VStack(spacing: 0) {
-                    
-                    HStack {
-                        
-                        Text("마이페이지")
-                            .font(.system(size: 20, weight: .semibold))
-                        
-                        Spacer()
-                        
-                        Button {
-                             
-                            // TODO: 설정
-                            
-                        } label: {
-                            Image.makeImageFromBundle(bundle: .module, name: "gear_icon", ext: .png)
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 28)
-                        }
-                        
-                    }
-                    .frame(height: 56)
-                    .padding(.horizontal, 21)
-                    
-                    ProfileShortCapView(
-                        nickName: userInfo.nickName ?? "비지정 닉네임",
-                        platformDescription: {
-                            guard let loginType = userInfo.loginType else {
-                                return "처리되지 못한 플랫폼"
-                            }
-                            
-                            switch loginType {
-                            case "Kakao":
-                                return "카카오톡 회원"
-                            case "Apple":
-                                return "애플회원"
-                            default:
-                                fatalError("처리되지 못한 플랫폼 형식")
-                            }
-                            
-                        }()
-                    )
-                    
-                    // 내프로필 보기
-                    Button {
-                        
-                        withAnimation {
-                            showProfileDetail = true
-                        }
-                        
-                    } label: {
-                        
-                        Text("내 프로필 보기")
-                            .font(.system(size: 16, weight: .semibold))
-                            .foregroundStyle(.black)
-                            .frame(height: 40)
-                            .padding(.horizontal, 20)
-                            .background(
-                                RoundedRectangle(cornerRadius: 20)
-                                    .fill(.light_gray)
-                            )
-                    }
-                    
-                    // TODO: 최근 본 팟
-                        
-                    Spacer()
-                    
-                }
-            }
-            
-            if showProfileDetail {
+                Color.white.ignoresSafeArea(.all, edges: .top)
                 
-                VStack(spacing: 0) {
+                ScrollView {
                     
-                    ZStack {
-                        
-                        Color.white
-                        
-                        Text("내 프로필")
-                            .font(.system(size: 20, weight: .semibold))
+                    VStack(spacing: 0) {
                         
                         HStack {
                             
-                            Image(systemName: "chevron.left")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 22, height: 22)
-                                .contentShape(Rectangle())
-                                .onTapGesture {
-                                    withAnimation {
-                                        showProfileDetail = false
-                                    }
-                                }
+                            Text("마이페이지")
+                                .font(.system(size: 20, weight: .semibold))
                             
                             Spacer()
                             
+                            Button {
+                                 
+                                // TODO: 설정
+                                
+                            } label: {
+                                Image.makeImageFromBundle(bundle: .module, name: "gear_icon", ext: .png)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 28)
+                            }
+                            
                         }
+                        .frame(height: 56)
                         .padding(.horizontal, 21)
                         
+                        ProfileShortCapView(
+                            nickName: userInfo.nickName ?? "비지정 닉네임",
+                            platformDescription: {
+                                guard let loginType = userInfo.loginType else {
+                                    return "처리되지 못한 플랫폼"
+                                }
+                                
+                                switch loginType {
+                                case "Kakao":
+                                    return "카카오톡 회원"
+                                case "Apple":
+                                    return "애플회원"
+                                default:
+                                    fatalError("처리되지 못한 플랫폼 형식")
+                                }
+                                
+                            }()
+                        )
+                        
+                        // 내프로필 보기
+                        NavigationLink {
+                            
+                            ProfileDetailView(userInfo: userInfo)
+                                .navigationBarBackButtonHidden()
+                            
+                        } label: {
+                            
+                            Text("내 프로필 보기")
+                                .font(.system(size: 16, weight: .semibold))
+                                .foregroundStyle(.black)
+                                .frame(height: 40)
+                                .padding(.horizontal, 20)
+                                .background(
+                                    RoundedRectangle(cornerRadius: 20)
+                                        .fill(.light_gray)
+                                )
+                        }
+                        
+                        // TODO: 최근 본 팟
+                            
+                        Spacer()
+                        
                     }
-                    .frame(height: 56)
-                    
-                    SpotProfileDetailView(userInfo: userInfo)
                 }
-                .transition(.move(edge: .trailing))
             }
+        }
+    }
+}
+
+fileprivate struct ProfileDetailView: View {
+    
+    @Environment(\.dismiss) private var dismiss
+    
+    var userInfo: SpotUser
+    
+    var body: some View {
+        
+        VStack(spacing: 0) {
             
+            ZStack {
+                
+                Color.white
+                
+                Text("내 프로필")
+                    .font(.system(size: 20, weight: .semibold))
+                
+                HStack {
+                    
+                    Image(systemName: "chevron.left")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 22, height: 22)
+                        .contentShape(Rectangle())
+                        .onTapGesture {
+                            dismiss()
+                        }
+                    
+                    Spacer()
+                    
+                }
+                .padding(.horizontal, 21)
+                
+            }
+            .frame(height: 56)
+            
+            SpotProfileDetailView(userInfo: userInfo)
         }
     }
 }
